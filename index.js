00fd3186c17547a4f21db35800846cf5a014826a
Richie2810 <richiegoneill@gmail.com>
2021-04-20 20:32:40 +0200

    1 Player working

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const BG_COLOUR = '#001b47';
 const SNAKE_COLOUR = '#1cff3e';
 const FOOD_COLOUR = '#ffee00';
 
-const socket = io('http://localhost:3001')
+const socket = io('http://localhost:3000')
 
 socket.on('init', handleInit)
+socket.on('gameState', handleGameState)
+socket.on('gameOver', handleGameOver)
+
 
 let canvas, ctx
-
-
-const gameScreen = document.getElementById('gameScreen');
 
 function init() {
     canvas = document.getElementById('canvas')
     ctx = canvas.getContext('2d')
-    canvas.width = 600
-    canvas.height = 600
+    canvas.width = canvas.height = 600
 
     ctx.fillStyle = BG_COLOUR
     ctx.fillRect(0, 0, canvas.width, canvas.height)
@@ -30,10 +29,39 @@ function keydown(e) {
   }
 
 function paintGame (state) {
+    ctx.fillStyle = BG_COLOUR
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
     const food = state.food;
     const gridsize = state.gridsize
+    const size = canvas.width / gridsize
+
+    ctx.fillStyle = FOOD_COLOUR
+    ctx.fillRect(food.x * size, food.y * size, size, size)
+
+    paintPlayer(state.player, size, SNAKE_COLOUR)
+}
+
+function paintPlayer (playerState , size, colour) {
+ const snake = playerState.snake
+
+ ctx.fillStyle = colour
+
+ for (let cell of snake) {
+     ctx.fillRect(cell.x * size, cell.y * size, size, size)
+ }
 }
 
 function handleInit (msg){
-    console.log(msg)
+    console.log(msg, 'test')
+}
+
+function handleGameState (gameState) {
+    gameState = JSON.parse(gameState)
+    requestAnimationFrame(()=> paintGame(gameState))
+
+}
+
+function handleGameOver (){
+    alert('You Lose')
 }
